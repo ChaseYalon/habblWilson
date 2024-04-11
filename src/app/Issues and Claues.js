c8bc842a9c6ea157240e264f6afc7363e20b161e
ChaseYalon <28yalonc@mtps.us>
2024-04-11 14:30:29 -0400

    issues and clauses 2

--- src/app/Issues and Claues.js
+++ src/app/Issues and Claues.js
@@ -27,6 +27,8 @@ console.log();
 //FYI: I pulled all these numbers out of my ass, maybe go find real data?
 
 var abbortion=new Issue('Abortion',createDistFromFunction(x => x ** 2),[],false)
-var totalBan=new Clause('Total ban',-0.95,'It is hereby adoptd that all abbortions banned with only exeptions listed below',[/*TODO*/])
-var forTheLifeOfTheMother=new Clause('For the life of the mother',0.2,"This is an execption to any and all abbortion bans, allowing the procedure when the life of the mother is imineltey at stake",[])
-var forRapeAndIncest=new Cluase('For Rape and Incest',0,'This is an exceptioin to any and all Abbortion ban(s) when the pregencey was inntated through unlawfull rape or incest.')
+    var totalBan=new Clause('Total ban',-0.95,'It is hereby adoptd that all abbortions banned with only exeptions listed below',[sizWeekBan])
+    var forTheLifeOfTheMother=new Clause('For the life of the mother',0.2,"This is an execption to any and all abbortion bans, allowing the procedure when the life of the mother is imineltey at stake",[])
+    var forRapeAndIncest=new Clause('For Rape and Incest',0,'This is an exceptioin to any and all Abbortion ban(s) when the pregencey was inntated through unlawfull rape or incest.',[])
+    var sixWeekBan=new Clause('6 week ban',-0.8,'It is hereby adoped that any abbortions unless specificaly later exempted are band 6 weeks after fertilization',[totalBan])
+    var twelveWeekBan=new Clause('12 week ban',-0.65,'It is hereby adoped that all abortions unless otherwise expemted are banned 12 weeks from conception unless specificaly exempted')
